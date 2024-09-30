788488ef6c26b7011c5de671f32900f60f59bdb4
Dhanya MD <dhanya13md@gmail.com>
2024-09-30 22:42:29 +0530

    displaying  pods

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,18 +6,19 @@ import { useQuery } from "convex/react";
 import { api } from '@/convex/_generated/api';
 
 const Home = () => {
+  const TrendingPodcastData = useQuery(api.podcasts.getTrendingPodcasts)
   return (
     <div className='flex flex-col gap-9 mt-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
         <div className='podcast_grid'>
-          {podcastData.map(({ id, title, description, imgURL }) => (
+          {TrendingPodcastData?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard
-              key={id}
-              imgUrl={imgURL}
-              title={title}
-              description={description}
-              podcastId={id}
+              key={_id}
+              imgUrl={imageUrl}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
             />
           ))}
         </div>
