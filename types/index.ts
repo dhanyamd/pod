25f88f50ffceff2cd3bb66823b768096e100c5a0
Dhanya MD <dhanya13md@gmail.com>
2024-10-06 20:23:20 +0530

    fix

--- types/index.ts
+++ types/index.ts
@@ -101,7 +101,7 @@ export interface AudioProps {
 }
 
 export interface AudioContextType {
-  audioUrl: any;
+  audioUrl?: any;
   audio: AudioProps | undefined;
   setAudio: React.Dispatch<React.SetStateAction<AudioProps | undefined>>;
 }
